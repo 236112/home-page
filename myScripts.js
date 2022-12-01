691f645c932994175522696c68391a7272dd6df3
Brandon <60393877+236112@users.noreply.github.com>
2022-12-01 14:02:16 -0600

    Dec 1B

--- myScripts.js
+++ myScripts.js
@@ -37,3 +37,16 @@ function showNote(){
         show = false;
     }
  }
+
+//CALCULATOR (JAVASCRIPT)
+var showCalc = false;
+function showCalculator(){
+    if (showCalc == false){
+        document.getElementById("calc").style.opacity = 1;
+        showCalc = true;
+    }
+    else if (showCalc == true){
+        document.getElementById("calc").style.opacity = 0;
+        showCalc = false;
+    }
+ }
